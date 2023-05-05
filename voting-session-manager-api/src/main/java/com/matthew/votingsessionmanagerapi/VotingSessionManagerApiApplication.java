95bef517139c9abe9d6243ca24fc2c73394c652e
Caroline-Martins <martins.tabares@gmail.com>
2023-05-05 19:45:47 -0300

    added project

---
+++ voting-session-manager-api/src/main/java/com/matthew/votingsessionmanagerapi/VotingSessionManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.matthew.votingsessionmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingSessionManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingSessionManagerApiApplication.class, args);
+	}
+
+}
---
+++ voting-session-manager-api/src/test/java/com/matthew/votingsessionmanagerapi/VotingSessionManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matthew.votingsessionmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingSessionManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

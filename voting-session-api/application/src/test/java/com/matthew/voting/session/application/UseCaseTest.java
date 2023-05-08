5a0049270f24d29372aacdffca88a65a5aeb7ac3
Caroline-Martins <martins.tabares@gmail.com>
2023-05-08 20:02:52 -0300

    Added mockito dependency

--- voting-session-api/application/src/test/java/com/matthew/voting/session/application/UseCaseTest.java
+++ voting-session-api/application/src/test/java/com/matthew/voting/session/application/UseCaseTest.java
@@ -1,14 +1,14 @@
 package com.matthew.voting.session.application;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 public class UseCaseTest {
 
     @Test
     public void testExecuteUseCase() {
-        Assertions.assertNotNull(new UseCase());
-        //Assertions.assertNotNull(new UseCase().execute());
     }
 
 }
